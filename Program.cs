cbf9b70a45f46200936bfce4f810d35a2ae80bd4
= <=>
2023-02-09 11:42:20 +0300

    first variable in C#

--- Program.cs
+++ Program.cs
@@ -1,4 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("The current time is " + DateTime.Now);
-Console.WriteLine("I launched first app in VS code ");
+// Console.WriteLine("Hello, World!");
+// Console.WriteLine("The current time is " + DateTime.Now);
+// Console.WriteLine("I launched first app in VS code ");
+Console.Write("Введите Ваше Имя:");
+string username = Console.ReadLine();
+Console.Write("Привет, ");
+Console.WriteLine(username);
