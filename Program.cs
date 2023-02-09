f4e8c7cb4937926913d6bced32fb20a02805b75a
= <=>
2023-02-09 16:30:27 +0300

    created script for sumarize of two numbers, requested from user

--- Program.cs
+++ Program.cs
@@ -2,7 +2,13 @@
 // Console.WriteLine("Hello, World!");
 // Console.WriteLine("The current time is " + DateTime.Now);
 // Console.WriteLine("I launched first app in VS code ");
-Console.Write("Введите Ваше Имя:");
-string username = Console.ReadLine();
-Console.Write("Привет, ");
-Console.WriteLine(username);
+//Console.Write("Введите Ваше Имя:");
+//string username = Console.ReadLine();
+//Console.Write("Привет, ");
+//Console.WriteLine(username);
+Console.WriteLine("Введите первое число");
+int first_number = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int second_number = int.Parse(Console.ReadLine());
+int result = first_number + second_number;
+Console.WriteLine(result + " Результат вычислений суммы из " +first_number + " и " +second_number);
