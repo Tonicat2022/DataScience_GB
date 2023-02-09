8ee200ff81690ae9e354723ec003518cf80253fb
= <=>
2023-02-09 23:33:25 +0300

    status update

---
+++ C#lectures/Lectures2/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+// Console.WriteLine("The current time is " + DateTime.Now);
+// Console.WriteLine("I launched first app in VS code ");
+//Console.Write("Введите Ваше Имя:");
+//string username = Console.ReadLine();
+//Console.Write("Привет, ");
+//Console.WriteLine(username);
+Console.WriteLine("Input please first number");
+double first_number = double.Parse(Console.ReadLine());
+Console.WriteLine("Input please second number");
+double second_number = double.Parse(Console.ReadLine());
+double result = first_number + second_number;
+Console.WriteLine(result + " Result of summ of " +first_number + " and " +second_number);
