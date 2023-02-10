8c8a46b621c8bac6cecb93dce96b88eaacda13ca
= <=>
2023-02-10 17:23:12 +0300

    Runningdog started*

--- C#Practice/RunningDog/Program.cs
+++ C#Practice/RunningDog/Program.cs
@@ -1,13 +1,14 @@
 ﻿Console.Clear();
-int count = 0, close = 0, time = 0, check = 0;
-// Console.WriteLine("Input initial distance between friends = ");
-// double distance = double.Parse(Console.ReadLine());
-// Console.WriteLine("Input Dog speed = ");
-// double dogSpeed = double.Parse(Console.ReadLine());
-// Console.WriteLine("Input First friend speed = ");
-// double firstFriendSpeed = double.Parse(Console.ReadLine());
-// Console.WriteLine("Input Second friend speed = ");
-// double SecondFriendSpeed = double.Parse(Console.ReadLine());
+int count = 0, close = 0, check = 0;
+double time = 0;
+Console.WriteLine("Input initial distance between friends = ");
+double distance = double.Parse(Console.ReadLine());
+Console.WriteLine("Input Dog speed = ");
+double dogSpeed = double.Parse(Console.ReadLine());
+Console.WriteLine("Input First friend speed = ");
+double firstFriendSpeed = double.Parse(Console.ReadLine());
+Console.WriteLine("Input Second friend speed = ");
+double SecondFriendSpeed = double.Parse(Console.ReadLine());
 Console.WriteLine("With which friend the dog was initially 1 or 2 - please input = ");
 int friend = int.Parse(Console.ReadLine());
 while (check !=1) // checking for correct friend number
