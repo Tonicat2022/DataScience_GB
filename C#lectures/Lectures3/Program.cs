b48ad7092ad4b5bd4eb7baaeebb54e0e3c4f28be
= <=>
2023-02-10 13:30:08 +0300

    add check if maria or masha

--- C#lectures/Lectures3/Program.cs
+++ C#lectures/Lectures3/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿ Console.Clear();
+ Console.Write("Input please name of the User : ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "masha" | username.ToLower() == "maria")
+{
+    Console.WriteLine("Hi there, dear Maria!!!");
+}
+else
+{
+    Console.Write("Hi, ");
+    Console.WriteLine(username);
+}
