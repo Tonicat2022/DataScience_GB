9ed7becd07741716daba349ca0eacff3c92d3d03
= <=>
2023-02-10 20:53:48 +0300

    seminar1 three tasks done

--- C#Seminars/Seminars/Seminar1/Program.cs
+++ C#Seminars/Seminars/Seminar1/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿    // first task
+//Console.Clear();
+//  Console.WriteLine("Please input any number");
+//  int num = 5;
+//  int num2 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine(num + ", " + num2);
+    // second task
+// Console.Clear();
+// Console.WriteLine("Please input any number");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int squate = num * num;
+// //Console.WriteLine("Squate of " + num + " = " + squate);
+// Console.WriteLine($"Squate of {num} is {squate}");
+    // third task
+// Console.Clear();
+// Console.WriteLine("Please input squate of the number");
+// int num = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Please input number");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+// int square = num2 * num2;
+// if (num == square)
+//     {
+//     Console.WriteLine($"{num} is  square of the {num2}");
+//     }
+// else
+//     {
+//     Console.WriteLine($"{num} is NOT square of the {num2}");
+//     };
+    // fourth task
