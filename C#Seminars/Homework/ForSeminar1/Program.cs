7318b58ec4bb0c81ec6faebe1741d0aacc0d725c
Tonicat2022 <anton.tselykovskiy@gmail.com>
2023-02-11 20:45:17 +0300

    removed some files to Privat

--- C#Seminars/Homework/ForSeminar1/Program.cs
+++ C#Seminars/Homework/ForSeminar1/Program.cs
@@ -43,21 +43,21 @@
 //   Console.WriteLine($"Number {n} is devided by 2 with rest: {rest}");
 // };
     // task 8
-// Console.Clear();
-// Console.WriteLine("Please input Number");
-// int n = Convert.ToInt32(Console.ReadLine());
-// int current = 2;
-// if(n < 2)
-// {
-//     Console.WriteLine($"Where no positive even numbers between 1 and {n} ");    
-// }
-// else
-// {   
-//     Console.Write($"Positive Even numbers between 1 and {n} are: ");
-//     while (current <= n)
-//     {  
-//         Console.Write($"{current} ");
-//         current= current + 2;
-//     };
+Console.Clear();
+Console.WriteLine("Please input Number");
+int n = Convert.ToInt32(Console.ReadLine());
+int current = 2;
+if(n < 2)
+{
+    Console.WriteLine($"Where no positive even numbers between 1 and {n} ");    
+}
+else
+{   
+    Console.Write($"Positive Even numbers between 1 and {n} are: ");
+    while (current <= n)
+    {  
+        Console.Write($"{current} ");
+        current= current + 2;
+    };
 
-// };
+};
