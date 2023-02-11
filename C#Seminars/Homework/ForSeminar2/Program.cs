802412c269fabd3ab8d38b6d394ec0c0feea5a29
Tonicat2022 <anton.tselykovskiy@gmail.com>
2023-02-11 16:29:54 +0300

    net test folder under home pc were created

---
+++ C#Seminars/Homework/ForSeminar2/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+Console.WriteLine("Please input Number");
+int n = Convert.ToInt32(Console.ReadLine());
+int current = 2;
+if(n < 2)
+{
+    Console.WriteLine($"Where no positive even numbers between 1 and {n} ");    
+}
+else
+{   
+    Console.Write($"Positive Even numbers between 1 and {n} are: ");
+    while (current <= n)
+    {  
+        Console.Write($"{current} ");
+        current= current + 2;
+    };
+
+};
