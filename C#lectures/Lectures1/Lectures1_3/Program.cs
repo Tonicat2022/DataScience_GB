a32ca29af60e5d1f02be4717a829817f853e3400
= <=>
2023-02-13 13:33:23 +0300

    changed names of folders

---
+++ C#lectures/Lectures1/Lectures1_3/Program.cs
@@ -0,0 +1,13 @@
+﻿ Console.Clear();
+ Console.Write("Input please name of the User : ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "masha" | username.ToLower() == "maria")
+{
+    Console.WriteLine("Hi there, dear Maria!!!");
+}
+else
+{
+    Console.Write("Hi, ");
+    Console.WriteLine(username);
+}
