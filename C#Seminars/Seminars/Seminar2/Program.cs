a2c479c3f813e542a89cf8bd53b831a26f65a861
= <=>
2023-02-14 21:09:59 +0300

    Practice1 task 4

--- C#Seminars/Seminars/Seminar2/Program.cs
+++ C#Seminars/Seminars/Seminar2/Program.cs
@@ -30,44 +30,49 @@
 // Console.WriteLine(Random_number);
 // Console.WriteLine(DeleteDecimal(Random_number));
 
+// // Task 4
+// Console.Clear();
+// void Divided_7and23 (int num)
+// {
+//     if (num % 7 == 0 && num % 23 == 0)
+//     {
+//         Console.WriteLine($"Yes, {num} divided by 7 and 23 without any rest");
+//     }
+//     else
+//     {
+//         int TheRest7 = num % 7; int TheRest23 = num % 3;
+//         Console.WriteLine($"No, {num} the rest of 7 is {TheRest7} and the rest of 23 is {TheRest23}");
+//     };
+
+// };
+
+// Console.WriteLine("Please input number");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// Divided_7and23(num);
+
 // Task 4
 Console.Clear();
-void Divided_7and23 (int num)
+void ifDividedWithZero (int num, int num2)
 {
-    if (num % 7 == 0 && num % 23 == 0)
+    if (num % num2 == 0)
     {
-        Console.WriteLine($"Yes, {num} divided by 7 and 23 without any rest");
+        Console.WriteLine($"{num} divided by {num2} without any rest");
     }
     else
     {
-        int TheRest7 = num % 7; int TheRest23 = num % 3;
-        Console.WriteLine($"No, {num} the rest of 7 is {TheRest7} and the rest of 23 is {TheRest23}");
-    };
-
+        int TheRest = num % num2;
+        Console.WriteLine($" The rest of divide {num} by {num2} is {TheRest}");   
+    }
 };
 
-Console.WriteLine("Please input number");
-int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Please inter first number");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Please inter second number");
+int b = Convert.ToInt32(Console.ReadLine());
 
-Divided_7and23(num);
-
-// Task 3
-// Console.Clear();
-// int ifDividedWithZero (int num, int num2)
-// {
-//     if (num % num2 == 0)
-//     {
-//         return  
-//     };
-// };
-
-// Console.WriteLine("Please inter first number");
-// int a = Convert.ToInt32(Console.ReadLine());
-// Console.WriteLine("Please inter second number");
-// int b = Convert.ToInt32(Console.ReadLine());
-
-// Console.WriteLine($"First number is {a}; Second  number is {b}");
-// Console.WriteLine(DeleteDecimal(Random_number));
+Console.WriteLine($"First number is {a}; Second  number is {b}");
+ifDividedWithZero(a,b);
 
 
 
