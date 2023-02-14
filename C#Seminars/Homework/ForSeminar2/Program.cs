003fb01e225b3f228abb71ad4f5145768999b58f
= <=>
2023-02-14 22:50:39 +0300

    Homework Seminar2 task 15 and all HW is done

--- C#Seminars/Homework/ForSeminar2/Program.cs
+++ C#Seminars/Homework/ForSeminar2/Program.cs
@@ -31,34 +31,60 @@
 // Console.WriteLine(LengthOfDigit(n));
 // MiddleDigit(n);
 
-    // Task10
+    // Task13
+// Console.Clear();
+
+// Console.WriteLine("Please input any number");
+// int n = Convert.ToInt32(Console.ReadLine());
+
+// int LengthOfDigit (int num)
+// {
+//     int index = 0;
+//         while (num % 10 >=1)
+//     {
+//         num = num /10;
+//         index++;
+//     }
+//     return index;
+// }
+// int Length = LengthOfDigit(n);
+
+// void ThirdNumberFormRight (int num)
+// {
+//     if (Length < 3)
+//     {
+//         Console.WriteLine($"There is NOT third digit in {num}");
+//     }
+//     else
+//     {
+//         Console.WriteLine($"{num % 1000 / 100} is third digit from right");
+//     };
+// };
+// Console.WriteLine($"{n} is {LengthOfDigit(n)} digit number");
+// ThirdNumberFormRight(n);
+
+ // Task15
 Console.Clear();
 
-Console.WriteLine("Please input any number");
-int n = Convert.ToInt32(Console.ReadLine());
-
-int LengthOfDigit (int num)
+void IfWeekend ( int num)
 {
-    int index = 0;
-        while (num % 10 >=1)
+    if (num <8 && num >0) // checking for appropriate number
     {
-        num = num /10;
-        index++;
-    }
-    return index;
-}
-int Length = LengthOfDigit(n);
-
-void ThirdNumberFormRight (int num)
-{
-    if (Length < 3)
-    {
-        Console.WriteLine($"There is NOT third digit in {num}");
+        if( num == 6 || num == 7)
+        {
+            Console.WriteLine($"Great!!! it is WEEKEND");
+        }
+        else
+        {
+            Console.WriteLine($"Unfortunatelly it is working day ((((");
+        }
     }
     else
     {
-        Console.WriteLine($"{num % 1000 / 100} is third digit from right");
-    };
-};
-Console.WriteLine($"{n} is {LengthOfDigit(n)} digit number");
-ThirdNumberFormRight(n);
+        Console.WriteLine($"{num} is not a appropriate number");
+    }
+}
+
+Console.WriteLine("Please input number day of the week");
+int n = Convert.ToInt32(Console.ReadLine());
+IfWeekend(n);
