e3796871f609f3d95bb5bd459aca85d1fdceccef
= <=>
2023-02-14 20:21:01 +0300

    Practice1 task 1

---
+++ C#Seminars/Seminars/Seminar2/Program.cs
@@ -0,0 +1,18 @@
+﻿// Task 1
+Console.Clear;
+void Max (int num)
+{
+    int ed = num % 10;
+    int dec = num /10;
+    if(ed>dec)
+    {
+        Console.WriteLine($" Biggest digit of {num} is {ed}");    
+    }
+    else
+    {
+        Console.WriteLine($" Biggest digit of {num} is {dec}");    
+    }
+}
+
+int Random_number = new Random().Next(10,100);
+Max(Random_number);
