aa972466024df7b03f3d4a29f6b617d257c5d098
= <=>
2023-02-17 21:26:56 +0300

    C# Seminars3 task3 and task4

--- C#Seminars/Seminars/Seminar3/Program.cs
+++ C#Seminars/Seminars/Seminar3/Program.cs
@@ -18,21 +18,52 @@
 // Console.WriteLine($"quater number {quater(x,y)}");
 
     // Distance between two points in 2-way space
-double x1, x2, y1, y2;
-Console.WriteLine("Please input X coordinate of First point]");
-x1 = Convert.ToDouble(Console.ReadLine());
-Console.WriteLine("Please input Y coordinate of First point]");
-y1 = Convert.ToDouble(Console.ReadLine());
-Console.WriteLine("Please input Z coordinate of First point]");
-x2 = Convert.ToDouble(Console.ReadLine());
-Console.WriteLine("Please input X coordinate of Second point]");
-y2 = Convert.ToDouble(Console.ReadLine());
+// double x1, x2, y1, y2;
+// Console.WriteLine("Please input X coordinate of First point]");
+// x1 = Convert.ToDouble(Console.ReadLine());
+// Console.WriteLine("Please input Y coordinate of First point]");
+// y1 = Convert.ToDouble(Console.ReadLine());
+// Console.WriteLine("Please input Z coordinate of First point]");
+// x2 = Convert.ToDouble(Console.ReadLine());
+// Console.WriteLine("Please input X coordinate of Second point]");
+// y2 = Convert.ToDouble(Console.ReadLine());
 
-void Distance (double x1, double x2, double y1, double y2)
-{
-    Console.WriteLine($"1-st point is {x1};{y1}");
-    Console.WriteLine($"1-st point is {x2};{y2}");
-    double result = Math.Round(Math.Sqrt(Math.Pow(x2-x1,2)+Math.Pow(y2-y1,2)),3);
-    Console.WriteLine($"Distance is {result}");
-};
-Distance(x1, x2, y1, y2);
+// void Distance (double x1, double x2, double y1, double y2)
+// {
+//     Console.WriteLine($"1-st point is {x1};{y1}");
+//     Console.WriteLine($"1-st point is {x2};{y2}");
+//     double result = Math.Round(Math.Sqrt(Math.Pow(x2-x1,2)+Math.Pow(y2-y1,2)),3);
+//     Console.WriteLine($"Distance is {result}");
+// };
+// Distance(x1, x2, y1, y2);
+
+    // Input x,y coordinate, output number of quater
+
+// string quater (int q)
+// {
+//     if( q == 1) return "x > 0 and y > 0";
+//     if( q == 2) return "x < 0 and y > 0";
+//     if( q == 3) return "x < 0 and y < 0";
+//     if( q == 4) return "x > 0 and y < 0";
+//     return "there is no such quater";
+// };
+// Console.WriteLine("Input quater number");
+// int q = Convert.ToInt16(Console.ReadLine());
+
+// Console.WriteLine($"range of coordinate: {quater(q)}");
+
+    // Input N and, qard 
+
+
+void qard (int num)
+{   
+    int index = 1;
+    while (index < num+1)
+    {
+        Console.WriteLine($"{index} - > {index*index};");
+        index ++;
+    };
+}
+Console.WriteLine("Please input number to see range of qard");
+int y = Convert.ToInt32(Console.ReadLine());
+qard(y);
