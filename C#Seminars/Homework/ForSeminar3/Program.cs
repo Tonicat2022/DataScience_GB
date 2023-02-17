4c355c135131f61b528e34ce1ffa1f33a4628c27
= <=>
2023-02-17 22:00:47 +0300

    homework3 is done add for loop to display array

--- C#Seminars/Homework/ForSeminar3/Program.cs
+++ C#Seminars/Homework/ForSeminar3/Program.cs
@@ -1,43 +1,41 @@
 ﻿        // Check for Polyndrome
-// int x = 0;
-// bool CheckIf5Digit (int num)
-// {
-//     if (num > 9999 && num < 100000)
-//     {
-//         return true; // yes, it is 
-//     }
-//         return false;
-// };
+int x = 0;
+bool CheckIf5Digit (int num)
+{
+    if (num > 9999 && num < 100000)
+    {
+        return true; // yes, it is 
+    }
+        return false;
+};
 
-// void CheckPolindrom (int num)
-// { 
-//     if (CheckIf5Digit(x))
-//     {
-//         char [] xArray = Convert.ToString(num).ToArray();
-//         // int index = 0; ///for display xArray
-//         // while (index < 5)
-//         // {
-//         //     Console.Write($"{xArray[index]};");
-//         //     index ++;
-//         // };
-//         if (xArray[0] == xArray[4] && xArray[1] == xArray[3])
-//         {
-//             Console.Write($"{num} is Polindrome");
-//         }
-//         else
-//         {
-//             Console.Write($"{num} is NOT Polindrome");
-//         }
-//     }
-//     else
-//     {
-//         Console.Write($"{num} is not Five-MiddleDigit Number");
-//     }
-// }
-// Console.WriteLine("Please input five-digit number");
-// x = Convert.ToInt32(Console.ReadLine());
-// Console.WriteLine();
-// CheckPolindrom(x);
+void CheckPolindrom (int num)
+{ 
+    if (CheckIf5Digit(x))
+    {
+        char [] xArray = Convert.ToString(num).ToArray();
+        // for (int i = 0; i < 5; i++)
+        // {
+        //     Console.Write($"{xArray[i]};");
+        // }; 
+        if (xArray[0] == xArray[4] && xArray[1] == xArray[3])
+        {
+            Console.Write($"{num} is Polindrome");
+        }
+        else
+        {
+            Console.Write($"{num} is NOT Polindrome");
+        }
+    }
+    else
+    {
+        Console.Write($"{num} is not Five-Digit Number");
+    }
+}
+Console.WriteLine("Please input five-digit number");
+x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+CheckPolindrom(x);
 
     // Distance between two points in 3-way space
 // double x1, x2, y1, y2, z1, z2;
@@ -65,15 +63,15 @@
 
 
     // Input N and raising N to the third power 
-void qard (int num)
-{   
-    int index = 1;
-    while (index < num+1)
-    {
-        Console.WriteLine($"{index} - > {index*index*index};");
-        index ++;
-    };
-}
-Console.WriteLine("Please input number to see raising it to the third power");
-int y = Convert.ToInt32(Console.ReadLine());
-qard(y);
+// void qard (int num)
+// {   
+//     int index = 1;
+//     while (index < num+1)
+//     {
+//         Console.WriteLine($"{index} - > {index*index*index};");
+//         index ++;
+//     };
+// }
+// Console.WriteLine("Please input number to see raising it to the third power");
+// int y = Convert.ToInt32(Console.ReadLine());
+// qard(y);
