b7585e7f121438a106d758dbce727855394a0f92
= <=>
2023-02-17 20:21:30 +0300

    C# Seminar3 task1

---
+++ C#Seminars/Seminars/Seminar3/Program.cs
@@ -0,0 +1,18 @@
+﻿    // Input x,y coordinate, output number of quater
+Console.WriteLine("Hello, World!");
+
+int quater (double x, double y)
+{
+    if( x > 0 && y > 0) return 1;
+    if( x < 0 && y > 0) return 2;
+    if( x < 0 && y < 0) return 3;
+    if( x > 0 && y < 0) return 4;
+    return 0;
+};
+Console.WriteLine("Input x");
+double x = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Input y");
+double y = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine($"quater number {quater(x,y)}");
