750fd4dd91e90263eccb3723bf688549ce41ee6d
= <=>
2023-02-20 21:03:46 +0300

    C#lectures\Lectures3 for cycle

--- C#lectures/Lectures3/Program.cs
+++ C#lectures/Lectures3/Program.cs
@@ -35,17 +35,90 @@
 // int year = Method3();
 // Console.WriteLine(year);
 
-    //type 4
-string Method4(int count, string text)
+//    // type 4
+// string Method4(int count, string text)
+// {
+//     int i = 0;
+//     string output = string.Empty;
+//     while (i < count)
+//     {
+//         output = output + text;
+//         i ++;
+//     };
+//     return  output;
+// }
+// string result = Method4(10,"asdf");
+// Console.WriteLine(result);
+
+    // for
+    // examble 1
+// string Method4(int count, string text)
+// {
+    
+//     string output = string.Empty;
+//     for (int i = 0; i < count; i++)
+//     {
+//         output = output + text;
+//     };
+//     return  output;
+// }
+// string result = Method4(10,"asdf");
+// Console.WriteLine(result);
+
+    // examble 2
+// void Method4(int x, int y)
+// {
+//     for (int i = 0; i < x; i = i+10)
+//     {
+//         for (int n = 1; n < y+1; n++)
+//         {
+//             Console.Write($"{i+n} ;");
+//         };
+//         Console.WriteLine();
+//     };
+// }
+// Method4(100,10);
+
+    // examble 3
+// "All the world's a stage! - and all the men and women merely players." 
+// "To thine own self be true." 
+// "Ay! -  there's the rub."
+// "Parting is such sweet sorrow!."
+// "If music be the food of love, play on."
+
+// string text = "All the worlds a stage! - and all the men and women merely players."
+//             +" To thine own self be true." 
+//             +"Ay! -  there's the rub. Parting is such sweet sorrow!"
+//             +"If music be the food of love, play on.";
+// string ReplaceSymbol (string text, char Old, char New)
+//  {
+//     string result = string.Empty;
+//     int length = text.Length;
+//     for (int i=0; i < length; i++)
+//     {
+//         if (text[i] == Old)
+//         {
+//         result = result + New;
+//         }
+//         else
+//         {
+//         result = result + text[i];
+//         };
+//     }
+//     return result;
+//  };
+// Console.WriteLine($"{ReplaceSymbol(text,'!',';')}");
+
+    // examble 4
+
+int[] arr = {1,5,4,3,2,6,7,1,1};
+
+void PrintArray (int[] array)
 {
-    int i = 0;
-    string output = string.Empty;
-    while (i < count)
+    int count = array.Length;
+    for ( int i = 0; i < count; i++)
     {
-        output = output + text;
-        i ++;
-    };
-    return  output;
+        Console.Write($"{array[1]};");
+    }
+    Console.WriteLine();
 }
-string result = Method4(10,"asdf");
-Console.WriteLine(result);
