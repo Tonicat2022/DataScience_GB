9a4df874c6520a4facf0538d5fc9ad66b9f2e693
= <=>
2023-02-21 21:34:18 +0300

    C#Seminars\Lectures4 all tasks done

--- C#Seminars/Seminars/Seminar4/Program.cs
+++ C#Seminars/Seminars/Seminar4/Program.cs
@@ -16,31 +16,57 @@
 
 // [1,0,1,1,0,1,0,0]
 
-int[] CreateRandomArray (int size, int minValue, int maxValue)
-{
-    int[] array = new int[size];
-    for (int i = 0; i < size; i++)
-    {
-        array[i] = new Random().Next(minValue, maxValue + 1); // high limit not included 
-    }
-    return array;
-}
+// int[] CreateRandomArray (int size, int minValue, int maxValue)
+// {
+//     int[] array = new int[size];
+//     for (int i = 0; i < size; i++)
+//     {
+//         array[i] = new Random().Next(minValue, maxValue + 1); // high limit not included 
+//     }
+//     return array;
+// }
 
-void Showarray ( int[] array)
-{
-    for (int i = 0; i < array.Length; i++)
-    {
-        Console.Write($"{array[i]}; ");
-    }
-    Console.WriteLine();
-}
-Console.WriteLine("Input array size");
-int size = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Input minimal value of array");
-int minValue = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Input maximum value of array");
-int maxValue = Convert.ToInt32(Console.ReadLine());
+// void Showarray ( int[] array)
+// {
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         Console.Write($"{array[i]}; ");
+//     }
+//     Console.WriteLine();
+// }
+// Console.WriteLine("Input array size");
+// int size = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input minimal value of array");
+// int minValue = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input maximum value of array");
+// int maxValue = Convert.ToInt32(Console.ReadLine());
 
-int[] myArray = CreateRandomArray(size, minValue, maxValue);
+// int[] myArray = CreateRandomArray(size, minValue, maxValue);
 
-Showarray(myArray);
+// Showarray(myArray);
+
+//     Factorial
+// int FindMulti (int x)
+// {
+//     int multi = 1;
+//     for ( int current = 1; current <= x; current++)
+//     {
+//         multi = multi * current; 
+//     }
+//     return multi;    
+// };
+// Console.WriteLine("Input X");
+// int X = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine($"{FindMulti(X)}");
+
+//Number of Digit in Number
+
+// int NumberOfDigit (int x)
+// {
+//     int count = 0;
+//     while (x>0) {x /= 10; count ++;}
+//     return count;
+// }
+// Console.WriteLine("Input X");
+// int X = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine($"{NumberOfDigit(X)}");
