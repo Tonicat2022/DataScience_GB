661b77e48fda00772a7248486ff417d2971211d8
= <=>
2023-02-26 22:24:49 +0300

    Lectures4

---
+++ C#lectures/Lectures4/Program.cs
@@ -0,0 +1,112 @@
+﻿// // 2way matrix(table with numbers)
+// void PrintArray(int[,] matr) //string[,] - table with string
+// {
+//  for (int i = 0; i < matr.GetLength(0); i++)
+//  {
+//  for (int j = 0; j < matr.GetLength(1); j++)
+//  {
+//  Console.Write($"{matr[i, j]} ");
+//  }
+//  Console.WriteLine();
+//  }
+// }
+
+// void FillArray(int[,] matr)
+// {
+//  for (int i = 0; i < matr.GetLength(0); i++)
+//  {
+//  for (int j = 0; j < matr.GetLength(1); j++)
+//  {
+//  matr[i,j] = new Random().Next(1,10);//[1; 10)
+//  }
+//  }
+// }
+
+// int[,] matrix = new int[3, 4]; // string[,] table = new string[3, 4] - for create table with string  
+
+// PrintArray(matrix);
+// FillArray(matrix);
+// Console.WriteLine();
+// PrintArray(matrix);
+
+
+//     // Thumbs up
+
+// int[,] pic = new int[,] //to create matrix
+// { 
+// {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, 
+// {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, 
+// {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, 
+// {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, 
+// {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, 
+// {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, 
+// {0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, 
+// {0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, 
+// {0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, 
+// {0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0 }, 
+// {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0 }, 
+// {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0 }, 
+// {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0 }, 
+// {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0 }, 
+// {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0 }, 
+// {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0 }, 
+// {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0 }, 
+// {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0 }, 
+// {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0 }, 
+// {0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0 }, 
+// {0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0 }, 
+// {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0 }, 
+// {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+// };
+
+// void PrintImage(int[,] image) //to print matrix as image
+// {
+//  for (int i = 0; i < image.GetLength(0); i++)
+//  {
+//  for (int j = 0; j < image.GetLength(1); j++)
+//  {
+//  if(image[i,j] == 0) Console.Write($" ");
+//  else Console.Write($"+");
+//  }
+//  Console.WriteLine();
+//  }
+// }
+// void FillImage(int row, int col) //to fill matrix as image
+// {
+//  if (pic[row, col] == 0) 
+//  {
+//  pic[row, col] = 1;
+//  FillImage(row - 1, col);
+//  FillImage(row, col - 1);
+//  FillImage(row + 1, col);
+//  FillImage(row, col + 1);
+//  }
+// }
+// PrintImage(pic);
+// FillImage(13, 13);
+// PrintImage(pic);
+
+//     // Factorial
+// double Factorial(int n)
+// {
+//  // 1! = 1
+//  // 0! = 1
+//  if(n == 1) return 1;
+//  else return n * Factorial(n-1);
+// }
+// for (int i = 1; i < 40; i++)
+// {
+//  Console.WriteLine($"{i}! = {Factorial(i)}");
+// }
+
+//     // Fibonacci
+// double Fibonacci(int n)
+// {
+//  if(n == 1 || n == 2) return 1;
+//  else return Fibonacci(n-1) + Fibonacci(n-2);
+// }
+// for (int i = 1; i < 50; i++)
+// {
+//  Console.WriteLine($"f({i}) = {Fibonacci(i)}");
+// }
+
