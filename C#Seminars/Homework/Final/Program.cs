b72e2387137b9695d52db58f64434b32a46fe44b
= <=>
2023-03-23 23:34:14 +0300

    C# Final Homework

--- C#Seminars/Homework/Final/Program.cs
+++ C#Seminars/Homework/Final/Program.cs
@@ -28,3 +28,29 @@
 // int b = Convert.ToInt32(Console.ReadLine());
 
 // Console.WriteLine(SumFromNToM(a, b));
+
+// task 68 - Summ from N to M 
+
+int Akkerman (int m, int n)
+{
+    if(m == 0)
+    {
+        return n+1;
+    }
+    if (m > 0 && n == 0)
+    {
+        return Akkerman (m-1, 1);
+    }
+    if (m > 0 && n > 0)
+    {
+        return Akkerman (m-1, Akkerman(m, n-1));
+    }
+    else return 0;
+}
+
+Console.WriteLine("Enter M of numbers to sum: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter N of numbers to sum: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(Akkerman(m, n));
