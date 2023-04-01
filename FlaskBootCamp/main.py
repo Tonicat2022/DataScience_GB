b441277a22eed767b247663254609acc3b6d4976
Tonicat2022 <anton.tselykovskiy@gmail.com>
2023-04-01 12:24:06 +0300

    BootCamp Flask web site created on local PC

--- FlaskBootCamp/main.py
+++ FlaskBootCamp/main.py
@@ -1,1 +1,17 @@
 from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def main():
+    return "<h1>Hello World<h1><br><a href='/index'>go to second page</a>"
+
+@app.route('/index')
+def index():
+    return "<h2>It is my first website<h2><br><a href='/'>Back to Homepage</a>"
+
+
+if __name__ == '__main__':
+    app.run()
