fcd3b77cd396fa651aa53bb57f28764a1f1fa3d0
Tonicat2022 <anton.tselykovskiy@gmail.com>
2023-04-07 22:57:17 +0300

    Python Seminar1 task 2,4,6 done

--- PythonBegin/Seminars/Seminar1/main.py
+++ PythonBegin/Seminars/Seminar1/main.py
@@ -17,10 +17,33 @@
 # print(f"{sumOfDigit(a)} is sum of digin it {a}")
 
 
-## task 4
+# task 4
 
 # a = int(input('please enter total quantity of shadoof'))
 # while a%6 != 0:
 #     print("There no way to find answer")
 #     a = int(input('please enter total quantity of shadoof'))
-# print(f"{a} -> {int(a/6)} {int(a/6*4)} {int(a/6)}") 
+# print(f"{a} -> {int(a/6)} {int(a/6*4)} {int(a/6)}")
+
+# task 6
+
+a = int(input('please enter 6-digit number'))
+while a < 100000 or a > 999999:
+    print("Entered not 6-digit number")
+    a = int(input('please enter 6-digit number'))
+    
+leftPart = a/1000
+rightPart = a % 1000
+
+
+def sumOfDigit(any):
+    x = int(any / 100)
+    y = int((any / 10) % 10)
+    z = int((any % 100) % 10)
+    sum = x+y+z
+    return sum
+
+if sumOfDigit(leftPart) == sumOfDigit(rightPart):
+    print("You have got Happy ticket!")
+else:
+    print("No, try again next time")
