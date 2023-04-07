7eef38d23f50ce2989dee624cb2736b222689ac6
Tonicat2022 <anton.tselykovskiy@gmail.com>
2023-04-07 22:46:51 +0300

    Python Seminar1 task 2,4 done

---
+++ PythonBegin/Lectures/Lectures1/main.py
@@ -0,0 +1,16 @@
+# a = 5
+# b = 5.98
+# c = 'hello'
+# print(a,' - ', b,' - ', c)
+# print(f"{a} - {b} - {c}")
+# print("{} - {} - {} ".format(a,b,c))
+
+# print("input first string")
+# a = input()
+# b = input("input second string")
+# print(a + b)
+
+c = 5.89
+print(c,type(c))
+c = int(c)
+print(c,type(c))
---
+++ PythonBegin/Seminars/Seminar1/main.py
@@ -0,0 +1,26 @@
+# task 2
+
+# a = int(input('please enter 3-digit number'))
+# while a < 100 or a > 999:
+#     print("Entered not 3-digit number")
+#     a = int(input('please enter 3-digit number'))
+
+
+# def sumOfDigit(any):
+#     x = int(any / 100)
+#     y = int((any / 10) % 10)
+#     z = int((any % 100) % 10)
+#     sum = x+y+z
+#     return sum
+
+
+# print(f"{sumOfDigit(a)} is sum of digin it {a}")
+
+
+## task 4
+
+# a = int(input('please enter total quantity of shadoof'))
+# while a%6 != 0:
+#     print("There no way to find answer")
+#     a = int(input('please enter total quantity of shadoof'))
+# print(f"{a} -> {int(a/6)} {int(a/6*4)} {int(a/6)}") 
